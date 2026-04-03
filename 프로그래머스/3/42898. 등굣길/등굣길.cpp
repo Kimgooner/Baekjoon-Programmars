abb8cef098ea21e885e9134ef0db01dec74be470
Whitedoggy <82828857+Kimgooner@users.noreply.github.com>
2026-04-03 22:44:48 +0900

    [level 3] Title: 등굣길, Time: 0.09 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int mod = 1000000007;
+int dp[105][105];
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+    for(vector<int> p : puddles){
+        dp[p[1]][p[0]] = -1;
+    }
+    
+    dp[1][1] = 1;
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if(i == 1 && j == 1) continue;
+            if(dp[i][j] == -1){
+                dp[i][j] = 0;
+            } else {
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % mod;
+            }
+        }
+    }
+    
+    return dp[n][m];
+}
